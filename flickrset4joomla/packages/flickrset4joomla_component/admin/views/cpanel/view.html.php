38db1fa60efe9f323e97140efbd2d430585cf295
Olivier De Groef <ogroef@users.noreply.github.com>
2014-12-08 13:37:19 +0100

    Refactoring the view/about folder name to view/cpanel folder

---
+++ flickrset4joomla/packages/flickrset4joomla_component/admin/views/cpanel/view.html.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *
+ * @version     $Id$
+ * @package     flickrset
+ * @subpackage  Content
+ * @copyright   Copyright (C) 2005-2014 Open Source Matters. All rights reserved.
+ * @license     GNU/GPL, see LICENSE.php
+ *
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * 
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla view library
+jimport('joomla.application.component.viewlegacy');
+
+/**
+ * Class Flickrset4JoomlaViewUploads
+ */
+class Flickrset4JoomlaViewAbout extends JViewLegacy {
+
+    /**
+     * Display method
+     *
+     * @param   string  $tpl  - the template
+     *
+     * @return mixed|void
+     */
+    public function display($tpl = null) {
+        $this->setToolbar();
+        parent::display($tpl);
+    }
+
+    /**
+     * Creates the toolbar options
+     *
+     * @return void
+     */
+    public function setToolbar() {
+        JToolBarHelper::title(JText::_('COM_FLICKRSET4JOOMLA_ABOUT_TOOLBAR_TITLE'));
+    }
+
+}
+
+?>
